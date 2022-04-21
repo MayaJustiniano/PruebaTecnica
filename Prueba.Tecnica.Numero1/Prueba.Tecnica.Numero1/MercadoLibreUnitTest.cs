0b0ad6e6e5ea4984bae9c0a809d951089abdfa95
MayaJustiniano <mayita.justiniano@gmail.com>
2022-04-21 11:27:59 -0400

    005 - Añadiendo el test para buscar un producto usado

--- Prueba.Tecnica.Numero1/Prueba.Tecnica.Numero1/MercadoLibreUnitTest.cs
+++ Prueba.Tecnica.Numero1/Prueba.Tecnica.Numero1/MercadoLibreUnitTest.cs
@@ -20,13 +20,13 @@ namespace Prueba.Tecnica.Numero1
             IWebDriver driverGH = new ChromeDriver(rutaSelenium);
 
             driverGH.Manage().Window.Maximize();
-           
+
             driverGH.Navigate().GoToUrl(direccionWeb);
 
             driverGH.FindElement(By.Id("BO")).SendKeys(Keys.Enter);
             driverGH.FindElement(By.Name("as_word")).SendKeys("Iphone");
             driverGH.FindElement(By.Name("as_word")).SendKeys(Keys.Enter);
-          
+
             #region Buscando el filtro llamado nuevo
             var contenedorPrincipal = driverGH.FindElement(By.ClassName("ui-search-filter-groups"));
             var filtrosDentroDeContenedor = contenedorPrincipal.FindElements(By.ClassName("ui-search-filter-dl"));
@@ -46,5 +46,39 @@ namespace Prueba.Tecnica.Numero1
 
             Assert.IsTrue(listaResultados.Count >= 3);
         }
+
+        [TestMethod]
+        public void BuscarPorFiltroUsado()
+        {
+            IWebDriver driverGH = new ChromeDriver(rutaSelenium);
+
+            driverGH.Manage().Window.Maximize();
+
+            driverGH.Navigate().GoToUrl(direccionWeb);
+
+            driverGH.FindElement(By.Id("BO")).SendKeys(Keys.Enter);
+            driverGH.FindElement(By.Name("as_word")).SendKeys("Iphone");
+            driverGH.FindElement(By.Name("as_word")).SendKeys(Keys.Enter);
+
+            #region Buscando el filtro llamado usado
+            var contenedorPrincipal = driverGH.FindElement(By.ClassName("ui-search-filter-groups"));
+            var filtrosDentroDeContenedor = contenedorPrincipal.FindElements(By.ClassName("ui-search-filter-dl"));
+            var filtroLlamadoCondicion = filtrosDentroDeContenedor[3];
+
+            var tagUl = filtroLlamadoCondicion.FindElement(By.TagName("ul"));
+            var filtroLlamadoUsado = tagUl.FindElements(By.TagName("li"))[1];
+
+            filtroLlamadoUsado.FindElement(By.ClassName("ui-search-button-link")).SendKeys(Keys.Enter);
+            #endregion
+
+            #region Comprobando la cantidad de listado
+            var contenedorPrincipalSearch = driverGH.FindElement(By.ClassName("ui-search-results"));
+            var contenedorResultados = contenedorPrincipalSearch.FindElement(By.TagName("ol"));
+            var listaResultados = contenedorResultados.FindElements(By.TagName("li"));
+            #endregion
+
+            Assert.IsTrue(listaResultados.Count >= 3);
+        }
+
     }
 }
