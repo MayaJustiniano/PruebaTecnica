dcd8d814658aa8b8447a1b5a0203cf10cfa884f3
MayaJustiniano <mayita.justiniano@gmail.com>
2022-04-20 21:47:23 -0400

    002 - Añadiendo nuevo Unit test

--- Prueba.Tecnica.Numero1/Prueba.Tecnica.Numero1/MercadoLibreUnitTest.cs
+++ Prueba.Tecnica.Numero1/Prueba.Tecnica.Numero1/MercadoLibreUnitTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Prueba.Tecnica.Numero1
 {
     [TestClass]
-    public class UnitTest1
+    public class MercadoLibreUnitTest
     {
         [TestMethod]
         public void TestMethod1()
