8e4589a73e451f677d24faa9b8957187f3213903
MayaJustiniano <mayita.justiniano@gmail.com>
2022-04-20 23:58:32 -0400

    004 - Añadiendo el test para buscar un producto nuevo

--- Prueba.Tecnica.Numero1/Prueba.Tecnica.Numero1/MercadoLibreUnitTest.cs
+++ Prueba.Tecnica.Numero1/Prueba.Tecnica.Numero1/MercadoLibreUnitTest.cs
@@ -1,14 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Prueba.Tecnica.Numero1
 {
     [TestClass]
     public class MercadoLibreUnitTest
     {
+        #region Variables Privadas
+        private readonly string rutaSelenium = "C:\\Selenium";
+        private readonly string direccionWeb = "https://mercadolibre.com/";
+
+        #endregion
         [TestMethod]
-        public void TestMethod1()
+        public void BuscarPorFiltroNuevo()
         {
+            IWebDriver driverGH = new ChromeDriver(rutaSelenium);
+
+            driverGH.Manage().Window.Maximize();
+           
+            driverGH.Navigate().GoToUrl(direccionWeb);
+
+            driverGH.FindElement(By.Id("BO")).SendKeys(Keys.Enter);
+            driverGH.FindElement(By.Name("as_word")).SendKeys("Iphone");
+            driverGH.FindElement(By.Name("as_word")).SendKeys(Keys.Enter);
+          
+            #region Buscando el filtro llamado nuevo
+            var contenedorPrincipal = driverGH.FindElement(By.ClassName("ui-search-filter-groups"));
+            var filtrosDentroDeContenedor = contenedorPrincipal.FindElements(By.ClassName("ui-search-filter-dl"));
+            var filtroLlamadoCondicion = filtrosDentroDeContenedor[3];
+
+            var tagUl = filtroLlamadoCondicion.FindElement(By.TagName("ul"));
+            var filtroLlamadoNuevo = tagUl.FindElements(By.TagName("li"))[0];
+
+            filtroLlamadoNuevo.FindElement(By.ClassName("ui-search-button-link")).SendKeys(Keys.Enter);
+            #endregion
+
+            #region Comprobando la cantidad de listado
+            var contenedorPrincipalSearch = driverGH.FindElement(By.ClassName("ui-search-results"));
+            var contenedorResultados = contenedorPrincipalSearch.FindElement(By.TagName("ol"));
+            var listaResultados = contenedorResultados.FindElements(By.TagName("li"));
+            #endregion
+
+            Assert.IsTrue(listaResultados.Count >= 3);
         }
     }
 }
