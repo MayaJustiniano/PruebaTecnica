a5e9d3560852c23c3ee2b66f092d3324d3174f89
MayaJustiniano <mayita.justiniano@gmail.com>
2022-04-20 21:41:13 -0400

    001 - Creando el proyecto

---
+++ Prueba.Tecnica.Numero1/Prueba.Tecnica.Numero1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Prueba.Tecnica.Numero1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prueba.Tecnica.Numero1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("675b1b4b-3920-42bc-ad89-35c021ae0a02")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
